8937c3dc652df239e867f77c9c8904fcf2e22f73
Daniel Moran <daniel.moran@tanium.com>
2017-05-04 13:25:54 -0500

    adjusting postinstall

--- scripts/install.js
+++ scripts/install.js
@@ -2,5 +2,5 @@ var fs = require('fs');
 var path = require('path');
 
 if (process.platform === 'linux') {
-    fs.chmodSync(path.join(__dirname, '..', 'bin', 'winexe_x64'), 777);
+    fs.chmodSync(path.join(__dirname, '..', 'bin', 'winexe_x64'), 755);
 }
